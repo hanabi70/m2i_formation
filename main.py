b4e1c5548feeb41f4df868a77bf5f366382053b2
Antoine <antoine.alain.cohen@gmail.com>
2025-04-08 17:53:01 +0200

    fix main env variable git uri

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 import mlflow
 import mlflow.projects
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(".env")
 
 def run():
-    uri = os.getenv('GIT_URL',"")
+    uri = os.getenv('GIT_URI',"")
     print(uri)
     version = os.getenv('GIT_BRANCH',"")
+    experiment_name = os.getenv('EXPERIMENT_NAME',"")
     env_manager = "local"
     parameters = {
         "n_estimators":100
@@ -16,7 +17,8 @@ def run():
         uri=uri,
         version=version,
         parameters=parameters,
-        env_manager=env_manager
+        env_manager=env_manager,
+        experiment_name=experiment_name,
     )
 
 if __name__ == "__main__":
