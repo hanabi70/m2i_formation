938ac9d115658640884a758e91350e91a47188b4
Antoine <antoine.alain.cohen@gmail.com>
2025-04-10 09:49:26 +0200

    fix prediction schema

--- src/application/routes/api_routes.py
+++ src/application/routes/api_routes.py
@@ -11,11 +11,11 @@ class ApiRoutes:
         self.router.post("/predict", response_model=PredictionOutput)(self.predict)
     
     async def predict(self,prediction_input:PredictionInput):
-        # prediction_args = {
-        #     'sepal length (cm)': prediction_input.sepal_length,
-        #     'sepal width (cm)': prediction_input.sepal_width,
-        #     'petal length (cm)': prediction_input.petal_length,
-        #     'petal width (cm)': prediction_input.petal_width
-        # }
-        prediction = self.mlflow_processor.predict_one(features = prediction_input.model_dump(mode="python"))
+        prediction_args = {
+            'sepal length (cm)': prediction_input.sepal_length,
+            'sepal width (cm)': prediction_input.sepal_width,
+            'petal length (cm)': prediction_input.petal_length,
+            'petal width (cm)': prediction_input.petal_width
+        }
+        prediction = self.mlflow_processor.predict_one(features = prediction_args)
         return PredictionOutput(prediction=prediction)
